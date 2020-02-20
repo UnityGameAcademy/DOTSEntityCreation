f4800103182c8f70891190ca8ce7bf55b6b3742b
Wilmer Lin <unitygameacademy@gmail.com>
2020-02-20 14:38:51 -0800

    Unity DOTS: Creating an Entity

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Mathematics;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Mesh unitMesh;
+    [SerializeField] private Material unitMaterial;
+
+    private void Start()
+    {
+        MakeEntity();
+    }
+
+    private void MakeEntity()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        EntityArchetype archetype = entityManager.CreateArchetype(
+            typeof(Translation),
+            typeof(Rotation),
+            typeof(RenderMesh),
+            typeof(RenderBounds),
+            typeof(LocalToWorld)
+            );
+
+        Entity myEntity = entityManager.CreateEntity(archetype);
+
+        entityManager.AddComponentData(myEntity, new Translation
+            {
+                Value = new float3(2f, 0f, 4f)
+            });
+
+        entityManager.AddSharedComponentData(myEntity, new RenderMesh
+            {
+                mesh = unitMesh,
+                material = unitMaterial
+            });
+    }
+}
